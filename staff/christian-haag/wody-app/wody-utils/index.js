3599f5803da7fd198d6471513dc55864ee1fb6f6
vanhaaggen <vanhaaggen@gmail.com>
2019-09-06 20:04:34 +0200

    add fisher yates algorithm

--- staff/christian-haag/wody-app/wody-utils/index.js
+++ staff/christian-haag/wody-app/wody-utils/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     validate: require('./validate'),
-    randomDate: require('./randomDate')
+    randomDate: require('./randomDate'),
+    randomator: require('./fisherYates')
 }
