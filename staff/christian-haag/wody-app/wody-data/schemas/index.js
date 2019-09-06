54aba20cc7d2b007415dfccc80edbd0ee350a01f
vanhaaggen <vanhaaggen@gmail.com>
2019-09-06 17:52:17 +0200

    update movement schema

--- staff/christian-haag/wody-app/wody-data/schemas/index.js
+++ staff/christian-haag/wody-app/wody-data/schemas/index.js
@@ -1,6 +1,4 @@
 module.exports = {
-    onerm: require('./onerm'),
-    vehicle: require('./vehicle'),
     user: require('./user'),
     workout: require('./workout'),
     movement: require('./movement')
