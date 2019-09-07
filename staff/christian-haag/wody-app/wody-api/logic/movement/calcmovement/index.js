23c09648caeb986bd793eafebd9cfc27554cbba0
vanhaaggen <vanhaaggen@gmail.com>
2019-09-07 11:27:40 +0200

    update business logic

--- staff/christian-haag/wody-app/wody-api/logic/movement/calcmovement/index.js
+++ staff/christian-haag/wody-app/wody-api/logic/movement/calcmovement/index.js
@@ -1,4 +1,4 @@
-const { validate } = require('wody-utils')
+const { validate, randomator } = require('wody-utils')
 const { models: { User, Movement, Workout } } = require('wody-data')
 const { random, floor } = Math
 /**
@@ -14,21 +14,30 @@ module.exports = function (id) {
     validate.string(id, 'id')
 
     return (async () => {
-        let wodies
-
-        const user = await User.findById(id)
+        //find user
+        const user = await User.findById(id).lean()
 
         if (!user) throw new Error('user does not exist')
-
+        //destructure needed queries
         const { gender, goal, fitnesslevel } = user
+        //find movement that match with queries
+        const movements = await Movement.find({ gender: gender, goal: goal, fitnesslevel: fitnesslevel }).lean()
+        debugger
+        //save id's in array
+        movements.forEach(movement => {
+            let repArr = movement.reps
+            randomator(repArr)
+            repArr.splice(1)
+        })
 
-        const movements = Movement.find({ gender: gender, goal: goal, fitnesslevel: fitnesslevel }).lean()
 
-        wodies = movements.id
+        //order ids randomly in array
+        randomator(movements)
+        debugger
+        //keep only the first 5 array values
+        const workouts = movements.splice(0, 5)
+        return workouts
 
-        for (let i = 0; i <= 6; i++) {
-
-        }
 
 
     })()
